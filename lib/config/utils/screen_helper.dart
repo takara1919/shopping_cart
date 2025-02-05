c63a8169a267b2839caa634fe3efd762a6b2872f
takara192 <dhmtri19@gmail.com>
2025-02-05 11:20:37 +0700

    Splash Screen

--- lib/config/utils/screen_helper.dart
+++ lib/config/utils/screen_helper.dart
@@ -23,4 +23,17 @@ class ScreenHelper {
     }
     return const EdgeInsets.all(16.0);
   }
+
+  static Size screenSize(BuildContext context) {
+    return MediaQuery.of(context).size;
+  }
+
+  static double screenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static double screenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
 }
--- lib/features/splash/view/splash_screen.dart
+++ lib/features/splash/view/splash_screen.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shopping_cart/config/utils/screen_helper.dart';
+import '../../../config/router/route_names.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
+
+  Future<void> navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (mounted) {
+      context.go(RouteNames.home);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = ScreenHelper.screenSize(context);
+    final year = DateTime.now().year;
+    final imageSie = ScreenHelper.isPhone(context) ? size.width * 0.25 : size.width * 0.2;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/cart.png',
+              width: imageSie,
+              height: imageSie,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 32,
+            child: Text(
+              '© $year, QSoft. All rights reserved.',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
